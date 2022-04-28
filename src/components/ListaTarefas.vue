3de5ce5b8eccd170cd68c1be3e24b8533f132631
dhiego <dhiego.azevedo17@gmail.com>
2022-04-28 11:19:57 -0300

    ajustes visuais

--- src/components/ListaTarefas.vue
+++ src/components/ListaTarefas.vue
@@ -8,21 +8,22 @@ export default {
 
 <template>
   <div class="listaKanban">
-    <div class="titulo">
+    <div class="titulo tituloPrincipal">
       <h3>{{ kanban.titulo }}</h3>
     </div>
     <ul>
       <li v-for="tarefa in kanban.itens" :key="tarefa.id">
-        {{ tarefa.titulo }}
-        <div v-if="kanban.kanbanNext">
+        <div class="titulo">{{ tarefa.titulo }}</div>
+        <div>Conteudo</div>
+        <div class="btns">
           <input
+            v-if="kanban.kanbanNext"
             type="button"
             value="next"
             @click="$emit('moveTarefa', kanban.name, kanban.kanbanNext, tarefa)"
           />
-        </div>
-        <div v-if="kanban.kanbanBack">
           <input
+            v-if="kanban.kanbanBack"
             type="button"
             value="back"
             @click="$emit('moveTarefa', kanban.name, kanban.kanbanBack, tarefa)"
@@ -39,26 +40,41 @@ export default {
   border-radius: 8px;
   min-height: 300px;
   min-width: 400px;
+  max-height: 90vh;
+  overflow-y: scroll;
   align-items: center;
   margin: 10px;
 }
 
 .titulo {
+  text-align: center;
+  font-weight: bold;
+}
+
+.tituloPrincipal {
   background-color: darkblue;
   color: white;
-  text-align: center;
 }
 
 li {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
   border-radius: 5px;
   background-color: white;
   min-height: 100px;
-  width: 92%;
+  width: 90%;
   margin: 3%;
   list-style: none;
-  padding: 5px;
+  padding: 2%;
 }
 ul {
   padding: 0;
+}
+
+.btns {
+  justify-content: end;
+  display: flex;
+  flex-direction: row;
 }
 </style>
