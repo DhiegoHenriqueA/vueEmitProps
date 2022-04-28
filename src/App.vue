2388e6b8f80d84ab37dc5fd67f86ca6596897873
dhiego <dhiego.azevedo17@gmail.com>
2022-04-28 09:53:09 -0300

    ajustes visuais

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,34 @@ export default {
   data() {
     return {
       kanban: {
+        backlog: {
+          name: "backlog",
+          titulo: "tarefas backlog",
+          itens: [
+            { id: 1, titulo: "item1", descritivo: "item 1 descritivo" },
+            { id: 2, titulo: "item2", descritivo: "item 2 descritivo" },
+            { id: 7, titulo: "item7", descritivo: "item 7 descritivo" },
+            { id: 8, titulo: "item8", descritivo: "item 8 descritivo" },
+          ],
+          kanbanBack: null,
+          kanbanNext: "todo",
+        },
         todo: {
           name: "todo",
           titulo: "tarefas todo",
           itens: [
-            { id: 1, titulo: "item1", descritivo: "item 1 descritivo" },
-            { id: 2, titulo: "item2", descritivo: "item 2 descritivo" },
+            { id: 3, titulo: "item3", descritivo: "item 3 descritivo" },
+            { id: 4, titulo: "item4", descritivo: "item 4 descritivo" },
           ],
-          kanbanBack: null,
+          kanbanBack: "backlog",
           kanbanNext: "done",
         },
         done: {
           name: "done",
           titulo: "tarefas done",
           itens: [
-            { id: 1, titulo: "item3", descritivo: "item 1 descritivo" },
-            { id: 2, titulo: "item4", descritivo: "item 2 descritivo" },
+            { id: 5, titulo: "item5", descritivo: "item 5 descritivo" },
+            { id: 6, titulo: "item6", descritivo: "item 6 descritivo" },
           ],
           kanbanBack: "todo",
           kanbanNext: null,
@@ -44,8 +56,17 @@ export default {
 </script>
 
 <template>
-  <ListaTarefas :kanban="kanban.todo" @moveTarefa="moveTarefa" />
-  <ListaTarefas :kanban="kanban.done" @moveTarefa="moveTarefa" />
+  <div class="flex">
+    <div v-for="itemKanban in kanban" :key="itemKanban.name">
+      <ListaTarefas :kanban="itemKanban" @moveTarefa="moveTarefa" />
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.flex {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
--- src/components/ListaTarefas.vue
+++ src/components/ListaTarefas.vue
@@ -7,26 +7,44 @@ export default {
 </script>
 
 <template>
-  <h3>{{ kanban.titulo }}</h3>
-  <ul>
-    <li v-for="tarefa in kanban.itens" :key="tarefa.id">
-      {{ tarefa.titulo }}
-      {{ kanban.kanbanNext }}
-      <div v-if="kanban.kanbanNext">
-        <input
-          type="button"
-          value="next"
-          @click="$emit('moveTarefa', kanban.name, kanban.kanbanNext, tarefa)"
-        />
-      </div>
-      {{ kanban.kanbanBack }}
-      <div v-if="kanban.kanbanBack">
-        <input
-          type="button"
-          value="back"
-          @click="$emit('moveTarefa', kanban.name, kanban.kanbanBack, tarefa)"
-        />
-      </div>
-    </li>
-  </ul>
+  <div class="listaKanban">
+    <div class="titulo">
+      <h3>{{ kanban.titulo }}</h3>
+    </div>
+    <ul>
+      <li v-for="tarefa in kanban.itens" :key="tarefa.id">
+        {{ tarefa.titulo }}
+        <div v-if="kanban.kanbanNext">
+          <input
+            type="button"
+            value="next"
+            @click="$emit('moveTarefa', kanban.name, kanban.kanbanNext, tarefa)"
+          />
+        </div>
+        <div v-if="kanban.kanbanBack">
+          <input
+            type="button"
+            value="back"
+            @click="$emit('moveTarefa', kanban.name, kanban.kanbanBack, tarefa)"
+          />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
+<style>
+.listaKanban {
+  padding: 2px 15px;
+  background-color: beige;
+  border-radius: 8px;
+  min-height: 300px;
+  min-width: 300px;
+  align-items: center;
+}
+
+.titulo {
+  background-color: darkblue;
+  color: white;
+  text-align: center;
+}
+</style>
